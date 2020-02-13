d8e8486f53f0dd29f4506f53fb7ed59b0b665f55
Naim <naimlatifi98@hotmail.com>
2020-02-13 23:09:13 +0100

    added webpack config and added test files for js and scss

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { hello, square } from "../src/js/util.js"
+import '../src/scss/index.scss'
+
+console.log("this is my first code in webpack nice with webpack")
+console.log(hello())
+console.log(square(4))
---
+++ src/js/util.js
@@ -0,0 +1,14 @@
+function hello() {
+    let variable = "hello"
+    return variable
+}
+
+function square(x) {
+    return x * x;
+}
+
+module.exports = {
+    hello,
+    square
+
+}
