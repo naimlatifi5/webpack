05b153f7d5eb0018e30672cdf7a1d119c92a2366
Naim <naimlatifi98@hotmail.com>
2020-03-08 14:50:02 +0100

    added loadash and multiple entry points with substition output file, result in console

--- demo/config/webpack.dev.js
+++ demo/config/webpack.dev.js
@@ -4,12 +4,14 @@ const path = require('path')
 // webpack's main configuration object
 module.exports = {
     // entry point where webpack will begin his work
-    entry: "./src/index.js",
+    entry: {
+        app1: "./src/index.js",
+        app2: "./src/main.js",
+    },
 
     // result bundle output
     output: {
         path: path.resolve(__dirname, '../dist'),
-        filename: "bundle.js"
-    },
-    mode: "development"
+        filename: "[name].bundle.js"
+    }
 }
--- demo/src/index.js
+++ demo/src/index.js
@@ -1,1 +1,4 @@
-console.log("hello there webpack 2")
+import _ from 'lodash'
+let arr1 = [1]
+let arr2 = [2]
+console.log("concat", _.concat(arr2, [44]) )
