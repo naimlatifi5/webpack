a5676a7a976091c2b3ad01cb43b51d1d4add06a8
Naim <naimlatifi98@hotmail.com>
2020-03-19 23:57:33 +0100

    fixed for dev server with hot-reloading

--- demo/config/webpack.dev.js
+++ demo/config/webpack.dev.js
@@ -7,5 +7,11 @@ const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
     mode: 'development',
-    devtool: 'inline-source-map'
+    devtool: 'inline-source-map',
+     // hot reloading
+    
+     devServer: {
+        // look for the file changes in dist
+        contentBase: './dist'
+      }
 })
