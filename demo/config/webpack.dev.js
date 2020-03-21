5db0bcfce063c83af41567f876d2be49faa0ebbb
Naim <naimlatifi98@hotmail.com>
2020-03-21 22:04:59 +0100

    updated readme

--- demo/config/webpack.dev.js
+++ demo/config/webpack.dev.js
@@ -7,6 +7,7 @@ const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
     mode: 'development',
+    // see error on the file
     devtool: 'inline-source-map',
      // hot reloading
     
